7df806d35b071b0a39c71db9fe88413769240790
Fu3rte <Fu3rte@users.noreply.github.com>
2026-04-15 21:09:36 +0800

    refactor: replace boilerplate with design system entry point

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import './index.css'
+
 function App() {
-  return null;
+  return (
+    <div className="app-shell">
+      <main className="app-content">
+        <div className="page-container">
+          {/* App content will be added by feature development */}
+        </div>
+      </main>
+    </div>
+  )
 }
 
-export default App;
+export default App
