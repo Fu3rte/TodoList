74a35422af13871c993196528ea8e6005c4edf18
Fu3rte <Fu3rte@users.noreply.github.com>
2026-04-16 11:18:24 +0800

    11

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -8,7 +8,7 @@ class Task(Model):
     title = fields.CharField(max_length=255)
     description = fields.TextField(null=True)
     category = fields.ForeignKeyField("models.Category", related_name="tasks", null=True)
-    priority = fields.CharEnumField(enum_class=Priority, null=True)
+    priority = fields.CharEnumField(enum_type=Priority, null=True)
     due_date = fields.DatetimeField(null=True)
     is_completed = fields.BooleanField(default=False)
     user = fields.ForeignKeyField("models.User", related_name="tasks")
